87aad02dbbd39e194febf8803b131cd4e693370e
drrid <tarek.dr.rid@gmail.com>
2023-04-03 16:21:26 +0100

    init

---
+++ conf.py
@@ -0,0 +1,304 @@
+import sqlalchemy as db
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+import datetime as dt
+from datetime import timedelta
+import numpy as np
+
+from dateutil import parser
+import csv
+import io
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+password = os.getenv('DB_PASSWORD')
+uri = os.getenv('URI')
+
+
+engine = db.create_engine(f'mysql+pymysql://root:{password}@{uri}')
+Base = declarative_base()
+db_session = db.orm.sessionmaker(bind=engine, autocommit=True)
+session = db_session()
+
+# Encounter Class -----------------------------------------------------------------------------------------------------------
+class Encounter(Base):
+
+    __tablename__ = "encounter"
+    encounter_id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    patient_id = db.Column(db.Integer(), db.ForeignKey("patient.patient_id"))
+    rdv = db.Column(db.DateTime())
+    note = db.Column(db.String(100), default='')
+    payment = db.Column(db.Integer(), default=0)
+    treatment_cost = db.Column(db.Integer(), default=0)
+
+    def __repr__(self):
+        return f'{self.encounter_id},{self.rdv},{self.note},{self.payment},{self.treatment_cost}'
+
+# Patient Class -----------------------------------------------------------------------------------------------------------
+class patient(Base):
+
+    __tablename__ = 'patient'
+    patient_id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    phone = db.Column(db.Integer())
+    date_of_birth = db.Column(db.Date())
+    encounters = relationship("Encounter")
+
+    def __repr__(self):
+        return f'{self.patient_id},{self.first_name},{self.last_name},{self.date_of_birth},{self.phone}'
+
+#---------------------------------------------------------------------------------------------------------------------------
+
+
+def init_db():
+    Base.metadata.create_all(engine)
+    Base.metadata.bind = engine
+
+
+def update_patient(patient_id, first_name=None, last_name=None, phone=None, date_of_birth=None):
+    try:
+        patient_to_update = session.query(patient).filter(patient.patient_id == patient_id).one()
+
+        if first_name is not None:
+            patient_to_update.first_name = first_name
+        if last_name is not None:
+            patient_to_update.last_name = last_name
+        if phone is not None:
+            patient_to_update.phone = phone
+        if date_of_birth is not None:
+            patient_to_update.date_of_birth = date_of_birth
+
+        session.commit()
+    except Exception as e:
+        print(e)
+        session.rollback()
+
+
+def save_to_db(record):
+    try:
+        session.add(record)
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        print(e) 
+
+def update_note(id, note):
+    try:
+        session.query(Encounter).filter(Encounter.encounter_id == id).update({Encounter.note: note})
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        print(e)
+
+def update_fee(id, fee):
+    try:
+        session.query(Encounter).filter(Encounter.encounter_id == id).update({Encounter.treatment_cost: fee})
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        print(e)
+
+def update_payment(id, payment):
+    try:
+        session.query(Encounter).filter(Encounter.encounter_id == id).update({Encounter.payment: payment})
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        print(e)
+
+def delete_encounter(encounter_id):
+    try:
+        encounter_to_delete = session.query(Encounter).filter(Encounter.encounter_id == encounter_id).one()
+        session.delete(encounter_to_delete)
+        session.commit()
+    except Exception as e:
+        print(e)
+        session.rollback()
+
+def select_one_first_name(first_name):
+    try:
+        return session.query(patient).filter_by(patient.first_name == first_name).one()
+    except Exception as e:
+        print(e)
+
+def select_one_id(id):
+    try:
+        return session.query(patient).filter(patient.patient_id == id).one()
+    except Exception as e:
+        print(e)
+
+def select_all(first_name):
+    try:
+        return session.query(patient).filter_by(patient.first_name == first_name).all()
+    except Exception as e:
+        print(e)
+
+def select_all_contains(first_name):
+    try:
+        return session.query(patient).filter(patient.first_name.contains(first_name))
+    except Exception as e:
+        print(e)
+
+def select_all_starts_with(q):
+    try:
+        return [r for r in session.query(patient).filter(patient.first_name.startswith(q))]
+    except Exception as e:
+        print(e)
+
+def select_all_starts_with_all_fields(fname, lname, phone):
+    try:
+        return [r for r in session.query(patient).filter(patient.first_name.startswith(fname),
+                                                        patient.last_name.startswith(lname),
+                                                        patient.phone.startswith(phone))]
+    except Exception as e:
+        print(e)
+
+def select_all_starts_with_phone(q):
+    try:
+        return [r for r in session.query(patient).filter(patient.phone.startswith(q))]
+    except Exception as e:
+        print(e)
+
+def select_all_starts_with_lname(q):
+    try:
+        return [r for r in session.query(patient).filter(patient.last_name.startswith(q))]
+    except Exception as e:
+        print(e)
+
+def select_all_encounters():
+    try:
+        return session.query(Encounter).all()
+    except Exception as e:
+        print(e)
+
+def select_encounter(q):
+    try:
+        return session.query(Encounter).filter(Encounter.rdv == q.rdv).one()
+    except Exception as e:
+        print(e)
+
+def select_all_pt_encounters(id):
+    try:
+        return [r for r in session.query(Encounter).filter(Encounter.patient_id == id).all()]
+    except Exception as e:
+        print(e)
+
+def select_week_encounters(start, end):
+    try:
+        return session.query(Encounter).filter(Encounter.rdv.between(start, end)).all()
+    except Exception as e:
+        print(e)
+
+def select_patient_encounters(id):
+    try:
+        return session.query(Encounter).filter(Encounter.patient_id == id).all()
+    except Exception as e:
+        print(e)
+
+def get_weekly_start_end(ind):
+    # print(ind)
+    if ind < 0:
+        today_date = dt.datetime.today() - timedelta(days=ind*-6)
+    else:
+        today_date = dt.datetime.today() + timedelta(days=ind*6)
+
+    SHIFTED_INDEX = {0:2, 1:3, 2:4, 3:5, 4:6, 5:0, 6:1}
+
+    today_index = today_date.weekday()
+    shifted_index = SHIFTED_INDEX[today_index]
+    current_week = today_date - timedelta(days=shifted_index)
+
+    hour_start = 0
+    minute_start = 0
+    day_start = current_week.day
+    year_start = current_week.year
+    month_start = current_week.month
+
+    current_week_start = dt.datetime(year_start, month_start, day_start, hour_start, minute_start)
+    current_week_end = current_week_start + timedelta(days=6)
+
+    day_end = current_week_end.day
+    year_end = current_week_end.year
+    month_end = current_week_end.month
+    hour_end = 23
+    minute_end = 59
+    current_week_end_final = dt.datetime(year_end, month_end, day_end, hour_end, minute_end)
+    
+    return (current_week_start, current_week_end_final)
+
+
+import numpy as np
+
+def get_weekly_encounters_csv(result):
+    rows = ['9:00', '9:20', '9:40', '10:00', '10:20', '10:40',
+            '11:00', '11:20', '11:40', '12:00', '12:20', '12:40',
+            '13:00', '13:20', '13:40', '14:00', '14:20', '14:40',
+            '15:00', '15:20', '15:40']
+
+    dict_row = {'9:0': 0, '9:20': 1, '9:40': 2, '10:0': 3, '10:20': 4, '10:40': 5,
+                '11:0': 6, '11:20': 7, '11:40': 8, '12:0': 9, '12:20': 10, '12:40': 11,
+                '13:0': 12, '13:20': 13, '13:40': 14, '14:0': 15, '14:20': 16, '14:40': 17,
+                '15:0': 18, '15:20': 19, '15:40': 20}
+
+    dict_column = {0: 2, 1: 3, 2: 4, 3: 5, 4: 6, 5: 0, 6: 1}
+
+    coor_array = []
+    patients = []
+
+    weekly_matrix = np.full((21, 7), dtype=object, fill_value='_')
+
+    if result is not None:
+        for r in result:
+            rdv_time = f'{r.rdv.hour}:{r.rdv.minute}'
+            row = dict_row[rdv_time]
+            clm = dict_column[r.rdv.weekday()]
+            coor_array.append([row, clm])
+            patient = select_one_id(r.patient_id)
+            patients.append(f'{patient.first_name} {patient.last_name} {patient.patient_id} {r.encounter_id}')
+
+        for i, (x, y) in enumerate(coor_array):
+            weekly_matrix[x, y] = patients[i]
+        print(weekly_matrix)
+        arr2 = weekly_matrix.flatten()
+
+        for i, v in enumerate(arr2):
+            index = int(i / 7)
+            rows[index] = rows[index] + ',' + str(v)
+
+        pretty = '\n'.join(rows)
+        return pretty
+
+
+def calculate_owed_amount(patient_id):
+    try:
+        patient_encounters = select_patient_encounters(patient_id)
+        total_treatment_cost = sum(encounter.treatment_cost for encounter in patient_encounters)
+        total_payments = sum(encounter.payment for encounter in patient_encounters)
+
+        owed_amount = total_treatment_cost - total_payments
+
+        return owed_amount
+    except Exception as e:
+        print(e)
+        return None
+
+
+init_db()
+
+
+# patient_id = 1
+# owed_amount = calculate_owed_amount(patient_id)
+# print(f"The owed amount for patient {patient_id} is: {owed_amount}")
+
+
+
+
+
+# start, end = get_weekly_start_end(0)
+# enc = select_week_encounters(start, end)
+# rows = get_weekly_encounters_csv(enc)
+# rows2 = get_weekly_encounters_csv2(enc)
+
---
+++ pms-frontend.py
@@ -0,0 +1,350 @@
+from textual.app import App
+from textual.screen import Screen
+from textual.widgets import Static, Footer, Header, Input, DataTable, Button
+from textual.containers import Container, Horizontal, Vertical
+from textual.reactive import reactive
+from textual import events
+import conf
+import csv
+import io
+import datetime as dt
+import calendar
+from dateutil import parser
+
+# Custom Calendar DataTable ---------------------------------------------------------------------------------------------------------------------------------------------
+class CalTable(DataTable):
+    def watch_cursor_cell(self, old, value):
+        if self.data is not None:
+            self.screen.change_week(self.screen.week_index)
+            selected_cell = self.data[self.cursor_cell.row][self.cursor_cell.column]
+            pt_table = self.screen.create_find_pt()
+            enc_table = self.screen.create_find_enc()
+
+            if self.cursor_cell.column > 0 and '_' not in selected_cell:
+                pt_id = int(selected_cell.split(' ')[-2])
+                selected_pts_list = conf.select_one_id(pt_id)
+                rows = csv.reader(io.StringIO(str(selected_pts_list)))
+                pt_table.add_rows(rows)
+                self.screen.show_encounters(pt_table.data[0][0])
+            else:
+                self.screen.show_patients()
+
+        return super().watch_cursor_cell(old, value)
+
+# Custom Patient DataTable ---------------------------------------------------------------------------------------------------------------------------------------------
+class PatientTable(DataTable):
+    def watch_cursor_cell(self, old, value):
+        # if self.cursor_cell.column > 0 and self.cursor_cell.row > 0:
+        self.screen.change_week(self.screen.week_index)
+        pt_id = self.data[self.cursor_cell.row][0]
+        enc_table = self.screen.create_find_enc()
+        self.screen.show_encounters(pt_id)
+        return super().watch_cursor_cell(old, value)
+
+# Custom Encounter DataTable ---------------------------------------------------------------------------------------------------------------------------------------------
+class EncounterTable(DataTable):
+    def watch_cursor_cell(self, old, value):
+        if self.cursor_cell.column in [2, 3, 4]:
+            self.screen.change_week(self.screen.week_index)
+            enc_notes = self.data[self.cursor_cell.row][self.cursor_cell.column]
+            self.screen.query_one('#notes').focus()
+        return super().watch_cursor_cell(old, value)
+
+# Calendar Screen --------------------------------------------------------------------------------------------------------------------------------------------------
+class Calendar(Screen):
+    BINDINGS = [("f5", "clear_all()", "Clear"),
+                ("f2", "modify_patient()", "Modify Patient"),
+                ("f9", "toggle_dark()", "Toggle Dark Mode")]
+
+    week_index = reactive(0)
+    selected_value = reactive([])
+    selected_value2 = reactive([])
+    pt_id = reactive(0)
+    
+    def compose(self):
+        yield Footer()
+        yield Container(
+                Vertical(Horizontal(
+                        Input('', placeholder='First Name', id='fname'),
+                        Input('', placeholder='Last Name', id='lname'),
+                        Input('', placeholder='Phone', id='phone'),
+                        Input('', placeholder='Date Of Birth', id='dob'),
+                        Button('Add', id='addpatient'),
+                        Button('Update', id='updatepatient'), id='inputs'),
+                    id='upper_cnt'
+                ),
+                Vertical(
+                    Horizontal(
+                        Vertical(PatientTable(fixed_columns=1, zebra_stripes=True, id='pt_table'),
+                                EncounterTable(fixed_columns=1, zebra_stripes=True, id='enc_table'),
+                                Input(placeholder='Notes...', id='notes'), 
+                                Static(id='feedback'),
+                                id='tables'),
+                                CalTable(fixed_columns=1, zebra_stripes=True, id='cal_table'),
+                        id='tables_cnt'
+                    ),
+                    id='lower_cnt'
+                ),
+                id='app_grid'
+            )
+
+    def on_mount(self):
+        self.change_week(self.week_index)
+        self.create_find_pt()
+        self.create_find_enc()
+        self.show_patients()
+
+    def action_clear_all(self):
+        for inp in self.query(Input):
+            inp.value = ''
+        self.show_patients()
+
+
+    def action_modify_patient(self):
+        table = self.query_one('#pt_table')
+        row = table.cursor_cell.row
+        patient_id = table.data[row][0]
+        self.query_one('#fname').value = table.data[row][1]
+        self.query_one('#lname').value = table.data[row][2]
+        self.query_one('#phone').value = table.data[row][4]
+        self.query_one('#dob').value = table.data[row][3]
+        self.pt_id = int(table.data[row][0])
+
+    def on_input_changed(self, event: Input.Changed):
+        if event.sender.id in ['fname', 'lname', 'phone']:
+            self.show_patients()
+
+    def show_encounters(self, pt_id):
+        if pt_id:
+            enc_table = self.create_find_enc()
+            selected_pts_list2 = conf.select_all_pt_encounters(int(pt_id))
+            if selected_pts_list2:
+                rows = csv.reader(io.StringIO("\n".join([str(r) for r in selected_pts_list2])))
+                for ro in rows:
+                    enc_table.add_row(*ro, height=int(len(ro[2])/20+1))
+
+    def show_patients(self):
+        table = self.create_find_pt()
+        fname = self.query_one('#fname').value
+        lname = self.query_one('#lname').value
+        phone = self.query_one('#phone').value
+        if phone.isdigit():
+            phone = int(phone)
+        else:
+            self.query_one('#phone').value = ''
+
+        selected_pts_list = conf.select_all_starts_with_all_fields(fname, lname, phone)
+        if selected_pts_list:
+            table.add_rows(csv.reader(io.StringIO("\n".join([str(r) for r in selected_pts_list]))))
+
+    def on_input_submitted(self, event: Input.Submitted):
+        if event.sender.id == 'notes':
+            enc_table = self.query_one('#enc_table')
+            pt_table = self.query_one('#pt_table')
+            cursor = enc_table.cursor_cell.column
+            encounter_id = int(enc_table.data[enc_table.cursor_cell.row][0])
+            pt_id = int(pt_table.data[pt_table.cursor_cell.row][0])
+
+            if cursor == 2:
+                note = self.query_one('#notes').value
+                conf.update_note(encounter_id, note)
+                self.show_encounters(pt_id)
+                self.query_one('#notes').value = ''
+            if cursor == 3:
+                cost = int(self.query_one('#notes').value)
+                conf.update_payment(encounter_id, cost)
+                self.show_encounters(pt_id)
+                self.query_one('#notes').value = ''
+            if cursor == 4:
+                fee = int(self.query_one('#notes').value)
+                conf.update_fee(encounter_id, fee)
+                self.show_encounters(pt_id)
+                self.query_one('#notes').value = ''    
+
+        if event.sender.id in ['fname', 'lname', 'phone', 'dob']:
+            table = self.query_one('#pt_table')
+            fname = self.query_one('#fname').value
+            lname = self.query_one('#lname').value
+            phone = self.query_one('#phone').value
+
+            if fname == '' and lname == '' and phone == '':
+                return
+            if '_' not in self.selected_value:
+                return
+            if not self.selected_value:
+                return
+            if len(table.data) == 0:
+                return
+            if len(table.data) > 1:
+                self.query_one('#pt_table').focus()
+                return
+            if len(table.data) == 1:
+                self.selected_value2 = [0, 0, 0]
+                self.submit_patient()
+
+    def on_button_pressed(self, event: Button.Pressed):
+        if event.sender.id == 'addpatient':
+            fname = str(self.query_one('#fname', Input).value).capitalize()
+            lname = str(self.query_one('#lname', Input).value).capitalize()
+            phone = int(self.query_one('#phone', Input).value)
+            date_of_birth = parser.parse(str(self.query_one('#dob', Input).value))
+
+            conf.save_to_db(conf.patient(first_name=fname, 
+                            last_name=lname, 
+                            phone=phone, 
+                            date_of_birth=date_of_birth))
+
+            self.query_one('#feedback').update(f'Patient Added: {fname} {lname}')
+            self.show_patients()
+
+        if event.sender.id == 'updatepatient':
+            patient_id = self.pt_id
+            fname = str(self.query_one('#fname', Input).value).capitalize()
+            lname = str(self.query_one('#lname', Input).value).capitalize()
+            phone = int(self.query_one('#phone', Input).value)
+            date_of_birth = parser.parse(str(self.query_one('#dob', Input).value))
+            conf.update_patient(patient_id, first_name=fname, last_name=lname, phone=phone, date_of_birth=date_of_birth)
+            self.query_one('#feedback').update(f'Patient updated: {fname} {lname}')
+            self.show_patients()
+
+    def change_week(self, week):
+        table = self.query_one('#cal_table')
+        table.clear()
+        table.columns = []
+        start_date, end_date = conf.get_weekly_start_end(week)
+        table.add_column('', width=7)
+        DAYS = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednsday', 'Thursday', 'Friday']
+
+        for c, d in enumerate(DAYS):
+            today = dt.datetime.today()
+            today_midnight = dt.datetime(today.year, today.month, today.day, 0, 0)
+            weekday = start_date + dt.timedelta(days=c)
+            month = calendar.month_abbr[weekday.month]
+
+            if weekday == today_midnight:
+                table.add_column(f'[bold yellow]{d} {weekday.day} {month}', width=22)
+            else:
+                table.add_column(f'[blue]{d} {weekday.day} {month}', width=22)
+        
+        encounters = conf.select_week_encounters(start_date, end_date)
+        csv_rows = conf.get_weekly_encounters_csv(encounters)
+        # self.query_one('#feedback').update(csv_rows)
+        with io.StringIO(csv_rows) as input_file:
+            rows = csv.reader(input_file)
+            for ro in rows:
+                table.add_row(*ro, height=2)
+
+    def on_key(self, event: events.Key):
+        if event.key == 'ctrl+left':
+            self.week_index -= 1
+            self.change_week(self.week_index)
+
+        if event.key == 'ctrl+right':
+            self.week_index += 1
+            self.change_week(self.week_index)
+        
+        if event.key == 'ctrl+delete':
+            table = self.query_one('#cal_table')
+            data = table.data[table.cursor_cell.row][table.cursor_cell.column]
+
+            if data != '_':
+                enc_id = int(data.split(' ')[-1])
+                pt_id = int(data.split(' ')[-2])
+                conf.delete_encounter(enc_id)
+                self.change_week(self.week_index)
+                self.show_encounters(pt_id)
+            
+        if event.key == 'space':
+            table = self.query_one('#cal_table')
+            if len(table.data[0]) > 5:
+                selected_v = table.data[table.cursor_cell.row][table.cursor_cell.column]
+                self.selected_value = [table.cursor_cell.row, table.cursor_cell.column, selected_v]
+                if selected_v == '_':
+                    self.create_find_pt()
+                    for inp in self.query(Input):
+                        inp.value = ''
+                    self.query_one('#fname').focus()
+
+            else:
+                selected_v2 = table.data[table.cursor_cell.row][table.cursor_cell.column]
+                self.selected_value2 = [table.cursor_cell.row, table.cursor_cell.column, selected_v2]
+                self.submit_patient()
+
+    def create_find_pt(self):
+        table = self.query_one('#pt_table')
+        table.clear()
+        table.columns = []
+        PT_CLMN = [['ID', 4], ['First Name', 18], ['Last Name', 18], ['Date of Birth', 14], ['Phone', 10]]
+        for c in PT_CLMN:
+            table.add_column(f'{c[0]}', width=c[1])
+        return table
+
+    def create_find_enc(self):
+        table = self.query_one('#enc_table')
+        table.clear()
+        table.columns = []
+        PT_CLMN = [['ID', 4], ['Encounter', 20], ['Note', 20], ['Payment', 10], ['Trt Cost', 10]]
+        for c in PT_CLMN:
+            table.add_column(f'{c[0]}', width=c[1])
+        return table
+
+    def submit_patient(self):
+        if ('_' not in self.selected_value) or not self.selected_value:
+            return
+        
+        table = self.query_one('#pt_table')
+        selected_patient_id = table.data[self.selected_value2[0]][0]
+        finaltime = self.calculate_rdvtime()
+        pt_encounter = conf.Encounter(patient_id=selected_patient_id, rdv=finaltime)
+        if not conf.select_encounter(pt_encounter):
+            conf.save_to_db(pt_encounter)
+            self.change_week(self.week_index)
+            self.show_encounters(table.data[0][0])
+
+    def calculate_rdvtime(self):
+        
+        INV_DICT_ROW = {
+            0: '9:0', 1: '9:20', 2: '9:40', 3: '10:0',
+            4: '10:20', 5: '10:40', 6: '11:0', 7: '11:20',
+            8: '11:40', 9: '12:0', 10: '12:20', 11: '12:40',
+            12: '13:0', 13: '13:20', 14: '13:40', 15: '14:0',
+            16: '14:20', 17: '14:40', 18: '15:0', 19: '15:20',
+            20: '15:40'
+        }
+
+        start_date, end_date = conf.get_weekly_start_end(self.week_index)
+        encounter_date = start_date + dt.timedelta(days=self.selected_value[1]-1)
+        hour, minute = INV_DICT_ROW.get(self.selected_value[0]).split(':')
+        return dt.datetime(encounter_date.year, encounter_date.month, encounter_date.day, int(hour), int(minute))
+
+    # def calculate_rdvtime(self):
+
+    #     INV_DICT_ROW = {0:'9:0',1:'9:30',2:'10:0',3:'10:30',
+    #     4:'11:0',5:'11:30',6:'12:0',7:'12:30',
+    #     8:'13:0',9:'13:30',10:'14:0',11:'14:30',
+    #     12:'15:0',13:'15:30'}
+
+    #     start_date, end_date = conf.get_weekly_start_end(self.week_index)
+    #     encounter_date = start_date + dt.timedelta(days=self.selected_value[1]-1)
+    #     hour, minute = INV_DICT_ROW.get(self.selected_value[0]).split(':')
+    #     return dt.datetime(encounter_date.year, encounter_date.month, encounter_date.day, int(hour), int(minute))
+        
+# ------------------------------------------------------------------------Main App-----------------------------------------------------------------------------------------
+class PMSApp(App):
+    BINDINGS = [("f5", "clear_all()", "Clear"),
+            ("f2", "modify_patient()", "Modify Patient"),
+            ("f9", "toggle_dark()", "Toggle Dark Mode")]
+    CSS_PATH = 'styling.css'
+    SCREENS = {"screen1": Calendar()}
+
+    def on_mount(self):
+        self.push_screen(self.SCREENS.get('screen1'))
+
+    def action_toggle_dark(self):
+        self.dark = not self.dark
+
+if __name__ == "__main__":
+    app = PMSApp()
+    app.run()
+
+ 
