27a9dea60b060adf816d8d59623be6df268a602b
drrid <tarek.dr.rid@gmail.com>
2023-04-05 14:09:29 +0100

    moved old code to Legacy folder

--- conf.py
+++ conf.py
@@ -1,11 +1,10 @@
 import sqlalchemy as db
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, declarative_base
 import datetime as dt
-from datetime import timedelta
-import numpy as np
 from dotenv import load_dotenv
 import os
+from sqlalchemy import and_, func
+from datetime import date, time, timedelta
 
 
 load_dotenv()
@@ -90,58 +89,16 @@ def delete_encounter(encounter_id):
         print(e)
         session.rollback()
 
-def select_one_first_name(first_name):
-    try:
-        return session.query(patient).filter_by(patient.first_name == first_name).one()
-    except Exception as e:
-        print(e)
-
-def select_one_id(id):
-    try:
-        return session.query(patient).filter(patient.patient_id == id).one()
-    except Exception as e:
-        print(e)
-
-def select_all(first_name):
-    try:
-        return session.query(patient).filter_by(patient.first_name == first_name).all()
-    except Exception as e:
-        print(e)
-
-def select_all_contains(first_name):
-    try:
-        return session.query(patient).filter(patient.first_name.contains(first_name))
-    except Exception as e:
-        print(e)
-
 def select_all_starts_with(**kwargs):
     try:
         filters = [getattr(patient, key).startswith(value) for key, value in kwargs.items()]
-        return [r for r in session.query(patient).filter(*filters)]
-    except Exception as e:
-        print(e)
-
-def select_all_encounters():
-    try:
-        return session.query(Encounter).all()
-    except Exception as e:
-        print(e)
-
-def select_encounter(q):
-    try:
-        return session.query(Encounter).filter(Encounter.rdv == q.rdv).one()
+        return [(str(r.patient_id), str(r.first_name), str(r.last_name), str(r.date_of_birth), str(r.phone)) for r in session.query(patient).filter(*filters)]
     except Exception as e:
         print(e)
 
 def select_all_pt_encounters(id):
     try:
-        return [r for r in session.query(Encounter).filter(Encounter.patient_id == id).all()]
-    except Exception as e:
-        print(e)
-
-def select_week_encounters(start, end):
-    try:
-        return session.query(Encounter).filter(Encounter.rdv.between(start, end)).all()
+        return [(str(r.encounter_id), str(r.rdv), str(r.note), str(r.payment), str(r.treatment_cost)) for r in session.query(Encounter).filter(Encounter.patient_id == id).all()]
     except Exception as e:
         print(e)
 
@@ -150,79 +107,6 @@ def select_patient_encounters(id):
         return session.query(Encounter).filter(Encounter.patient_id == id).all()
     except Exception as e:
         print(e)
-
-def get_weekly_start_end(ind):
-    # print(ind)
-    if ind < 0:
-        today_date = dt.datetime.today() - timedelta(days=ind*-6)
-    else:
-        today_date = dt.datetime.today() + timedelta(days=ind*6)
-
-    SHIFTED_INDEX = {0:2, 1:3, 2:4, 3:5, 4:6, 5:0, 6:1}
-
-    today_index = today_date.weekday()
-    shifted_index = SHIFTED_INDEX[today_index]
-    current_week = today_date - timedelta(days=shifted_index)
-
-    hour_start = 0
-    minute_start = 0
-    day_start = current_week.day
-    year_start = current_week.year
-    month_start = current_week.month
-
-    current_week_start = dt.datetime(year_start, month_start, day_start, hour_start, minute_start)
-    current_week_end = current_week_start + timedelta(days=6)
-
-    day_end = current_week_end.day
-    year_end = current_week_end.year
-    month_end = current_week_end.month
-    hour_end = 23
-    minute_end = 59
-    current_week_end_final = dt.datetime(year_end, month_end, day_end, hour_end, minute_end)
-    
-    return (current_week_start, current_week_end_final)
-
-
-import numpy as np
-
-def get_weekly_encounters_csv(result):
-    rows = ['9:00', '9:20', '9:40', '10:00', '10:20', '10:40',
-            '11:00', '11:20', '11:40', '12:00', '12:20', '12:40',
-            '13:00', '13:20', '13:40', '14:00', '14:20', '14:40',
-            '15:00', '15:20', '15:40']
-
-    dict_row = {'9:0': 0, '9:20': 1, '9:40': 2, '10:0': 3, '10:20': 4, '10:40': 5,
-                '11:0': 6, '11:20': 7, '11:40': 8, '12:0': 9, '12:20': 10, '12:40': 11,
-                '13:0': 12, '13:20': 13, '13:40': 14, '14:0': 15, '14:20': 16, '14:40': 17,
-                '15:0': 18, '15:20': 19, '15:40': 20}
-
-    dict_column = {0: 2, 1: 3, 2: 4, 3: 5, 4: 6, 5: 0, 6: 1}
-
-    coor_array = []
-    patients = []
-
-    weekly_matrix = np.full((21, 7), dtype=object, fill_value='_')
-
-    if result is not None:
-        for r in result:
-            rdv_time = f'{r.rdv.hour}:{r.rdv.minute}'
-            row = dict_row[rdv_time]
-            clm = dict_column[r.rdv.weekday()]
-            coor_array.append([row, clm])
-            patient = select_one_id(r.patient_id)
-            patients.append(f'{patient.first_name} {patient.last_name} {patient.patient_id} {r.encounter_id}')
-
-        for i, (x, y) in enumerate(coor_array):
-            weekly_matrix[x, y] = patients[i]
-        print(weekly_matrix)
-        arr2 = weekly_matrix.flatten()
-
-        for i, v in enumerate(arr2):
-            index = int(i / 7)
-            rows[index] = rows[index] + ',' + str(v)
-
-        pretty = '\n'.join(rows)
-        return pretty
 
 
 def calculate_owed_amount(patient_id):
@@ -239,19 +123,72 @@ def calculate_owed_amount(patient_id):
         return None
 
 
+def generate_time_slot(start_hour, start_minute, duration, count):
+    time_slots = []
+    current_time = time(hour=start_hour, minute=start_minute)
+
+    for _ in range(count):
+        next_time_minute = current_time.minute + duration
+        next_time_hour = current_time.hour + next_time_minute // 60
+        next_time = time(hour=next_time_hour % 24, minute=next_time_minute % 60)
+        time_slots.append((current_time, next_time))
+        current_time = next_time
+
+    return time_slots
+
+
+def generate_schedule(week_index):
+    today = date.today()
+    # Find the nearest Saturday (0 = Saturday, 1 = Sunday, etc.)
+    days_to_saturday = (5 - today.weekday()) % 7
+    start_date = today + timedelta(days=days_to_saturday) + timedelta(weeks=week_index - 1)
+    
+    # Generate time slots
+    time_slots = generate_time_slot(9, 0, 20, 21)
+    
+    schedule = []
+    
+    # Add days of the week with their respective dates
+    days_of_week = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    schedule.append((" ", *tuple(f"{days_of_week[i]} {start_date.day + i} {start_date.strftime('%B')}" for i in range(7))))
+    
+    for time_slot in time_slots:
+        slot_start, slot_end = time_slot
+        encounters_in_slot = ["_"] * 7  # Initialize empty encounters for the week
+
+        for i in range(7):
+            day_start = dt.datetime.combine(start_date + timedelta(days=i), slot_start)
+            day_end = dt.datetime.combine(start_date + timedelta(days=i), slot_end)
+            
+            encounters = session.query(Encounter, patient).join(patient).filter(
+                and_(
+                    Encounter.rdv >= day_start,
+                    Encounter.rdv < day_end
+                )
+            ).all()
+            
+            if encounters:
+                encounter, pat = encounters[0]  # Assuming only one encounter per time slot
+                encounters_in_slot[i] = f"{pat.first_name} {pat.last_name} {encounter.encounter_id} {pat.patient_id}"
+        
+        schedule.append((slot_start.strftime('%H:%M'), *encounters_in_slot))
+    
+    return schedule
+
+
 init_db()
 
 
-# patient_id = 1
-# owed_amount = calculate_owed_amount(patient_id)
-# print(f"The owed amount for patient {patient_id} is: {owed_amount}")
+
+# results = generate_schedule(0)
+# print(results)
+# for r in results:
+#     print(r)
 
 
+# patients = select_all_starts_with(first_name='abd')
+# print(patients)
+# for patient in patients:
+#     print(patient)
 
-
-
-# start, end = get_weekly_start_end(0)
-# enc = select_week_encounters(start, end)
-# rows = get_weekly_encounters_csv(enc)
-# rows2 = get_weekly_encounters_csv2(enc)
 
--- main.py
+++ main.py
@@ -12,19 +12,23 @@ from dateutil import parser
 
 # Calendar Screen --------------------------------------------------------------------------------------------------------------------------------------------------
 class Calendar(Screen):
+
+    BINDINGS = [("ctrl+right", "next_week", "Next Week")]
+
     def compose(self):
         self.table = DataTable()
         self.calendar_widget = DataTable(id='cal_table')
         self.encounter_widget = DataTable(id='enc_table')
         self.patient_widget = DataTable(id='pt_table')
+        self.patient_widget.cursor_type = 'row'
         self.inputs_container = Vertical(Horizontal(
-                                Input('', placeholder='First Name', id='fname'),
-                                Input('', placeholder='Last Name', id='lname'),
-                                Input('', placeholder='Phone', id='phone'),
-                                Input('', placeholder='Date Of Birth', id='dob'),
-                                Button('Add', id='addpatient'),
-                                Button('Update', id='updatepatient'), id='inputs'),
-                            id='upper_cnt')
+                                    Input('', placeholder='First Name', id='fname'),
+                                    Input('', placeholder='Last Name', id='lname'),
+                                    Input('', placeholder='Phone', id='phone'),
+                                    Input('', placeholder='Date Of Birth', id='dob'),
+                                    Button('Add', id='addpatient'),
+                                    Button('Update', id='updatepatient'), id='inputs'),
+                                id='upper_cnt')
         self.tables_container = Vertical(
                             Horizontal(
                                 Vertical(self.patient_widget,
@@ -37,32 +41,29 @@ class Calendar(Screen):
                             id='lower_cnt')
         
         self.footer_widget = Footer()
-        yield Container(self.inputs_container,
-                        self.tables_container,
-                        id='app_grid')
-
-
+        
+        yield Container(self.inputs_container, self.tables_container, id='app_grid')
+        yield self.footer_widget    
+    
     def on_mount(self):
+        self.patient_widget.add_columns('ID', 'First Name', 'Last Name', 'Date of Birth', 'Phone')
+        self.encounter_widget.add_columns('ID', 'Encounter', 'Note', 'Payment', 'Fee')
         self.show_calendar(0)
         self.show_patients(first_name='')
-        self.show_encounters(4)
 
+    def log_error(self, msg):
+        self.query_one('#feedback').update(f'[bold red]{str(msg)}')
+
+    def log_feedback(self, msg):
+        self.query_one('#feedback').update(f'[bold teal]{str(msg)}')
 
     def show_patients(self, **kwargs):
-        patients = conf.select_all_starts_with(**kwargs)
-        patients.insert(0, ('ID', 'First Name', 'Last Name', 'Date of Birth', 'Phone'))
-        patients = iter(patients)
-        table = self.query_one('#pt_table')
-        table.add_columns(*next(patients))
-        table.add_rows(patients)
+        patients = iter(conf.select_all_starts_with(**kwargs))
+        self.patient_widget.add_rows(patients)
 
     def show_encounters(self, pt_id):
-        encounters = conf.select_all_pt_encounters(pt_id)
-        encounters.insert(0, ('ID', 'Encounter', 'Note', 'Payment', 'Fee'))
-        encounters = iter(encounters)
-        table = self.query_one('#enc_table')
-        table.add_columns(*next(encounters))
-        table.add_rows(encounters)
+        encounters = iter(conf.select_all_pt_encounters(pt_id))
+        self.encounter_widget.add_rows(encounters)
 
     def show_calendar(self, week_index):
         schedule = iter(conf.generate_schedule(week_index))
@@ -70,14 +71,21 @@ class Calendar(Screen):
         table.add_columns(*next(schedule))
         table.add_rows(schedule)
 
-    def on_data_table_cell_selected(self, message: DataTable.CellSelected):
-        pass
+    # def on_data_table_cell_selected(self, message: DataTable.CellSelected):
+    #     if message.control.id == 'pt_table':
+    #         self.show_encounters(message.cell_key.)
+
+    def on_data_table_row_selected(self, message: DataTable.RowSelected):
+        self.encounter_widget.clear()
+        pt_id = int(self.patient_widget.get_row(message.row_key)[0])
+        self.show_encounters(pt_id)
+            
 
     
 
 # ------------------------------------------------------------------------Main App-----------------------------------------------------------------------------------------
 class PMSApp(App):
-    CSS_PATH = 'styling-test.css'
+    CSS_PATH = 'styling.css'
     SCREENS = {"screen1": Calendar()}
 
     def on_mount(self):
